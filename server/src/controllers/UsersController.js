6eac5256b0f3118ef2202ef6122fe855a5876be9
enoquetembe <enoquetembetembe10@gmail.com>
2023-02-06 18:19:48 -0300

    insert user into database

--- server/src/controllers/UsersController.js
+++ server/src/controllers/UsersController.js
@@ -1,17 +1,25 @@
+const { hash } = require('bcryptjs')
+
 const AppError  = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite')
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body
 
-    if(!name) {
-      throw new AppError('Name is  required!')
+    const database = await sqliteConnection()
+    const usersEmailAlreadyExists = await database.get('SELECT * FROM users WHERE email = (?)', 
+    [email])
+    
+    if(usersEmailAlreadyExists) {
+      throw new AppError('This email is already in use.')
     }
 
-    response.send({
-      name,
-      email,
-      password,
-    })
+    const hashedPassword = await hash(password, 8)
+
+    await database.run('INSERT INTO users(name, email, password) VALUES(?, ?, ?)',
+    [name, email, hashedPassword] )
+
+    return response.status(201).json({})
   }
 }
 
