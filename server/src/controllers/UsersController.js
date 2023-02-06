f19fffbfc9e72ba4cd69bd77bc728a6ff5b3d0c9
enoquetembe <enoquetembetembe10@gmail.com>
2023-02-05 21:10:00 -0300

    treating asuync errors

--- server/src/controllers/UsersController.js
+++ server/src/controllers/UsersController.js
@@ -1,6 +1,13 @@
+const AppError  = require('../utils/AppError')
+
+
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
+
+    if(!name) {
+      throw new AppError('Name is  required!')
+    }
 
     response.send({
       name,
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
+require('express-async-errors')
 const express = require('express')
+const AppError = require('./utils/AppError')
 
 const routes = require('./routes')
 
@@ -7,6 +9,22 @@ app.use(express.json())
 
 app.use(routes)
 
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'Error',
+      message: error.message
+
+    })
+
+    console.error(error)
+
+    return response.status(500).json({
+      status: "Error",
+      message: 'Internal server error',
+    })
+  }
+}) 
 
 const PORT = 3000
 app.listen(PORT, () => console.log('Server is running on port ' +PORT))
