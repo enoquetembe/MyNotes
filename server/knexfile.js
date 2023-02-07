92489689f16527ec9e1b7a9d92e56bcf9e6ea939
enoquetembe <enoquetembetembe10@gmail.com>
2023-02-07 16:35:32 -0300

    notes migration

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,9 +4,15 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    
     connection: {
       filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
+
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
+    },
+
     useNullAsDefault: true,
   },
 };
