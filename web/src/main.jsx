aa6c51438a1795ae263dd7acb587567c0d9a1529
enoquetembe <enoquetembetembe10@gmail.com>
2023-02-16 20:46:23 -0300

    routes

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { New } from './pages/New'
+import { Routes } from './routes'
 
 import { ThemeProvider } from 'styled-components'
 import  theme  from './styles/theme'
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
