dd4a5eb842b4108afa4e4031e3e5c98a9c32f7a2
enoquetembe <enoquetembetembe10@gmail.com>
2023-02-20 21:10:45 -0300

    correcting cors

--- server/src/server.js
+++ server/src/server.js
@@ -11,7 +11,9 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-app.use(cors)
+
+app.use(cors())
+
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER ))
 
 app.use(routes)
