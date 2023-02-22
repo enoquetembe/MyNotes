2b0d85ccd378a5325e844f6caa59590499db244f
enoquetembe <enoquetembetembe10@gmail.com>
2023-02-22 19:21:22 -0300

    route redirection

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -3,10 +3,14 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
+import { useAuth } from '../hooks/auth'
+
 export function Routes() {
+  const { user } = useAuth()
+
   return (
     <BrowserRouter>
-      <AuthRoutes/>
+      {user? <AppRoutes/> : <AuthRoutes/>}
     </BrowserRouter>
   )
 }
