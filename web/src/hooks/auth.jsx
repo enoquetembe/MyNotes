d360e92d8957f0795f7a0c21078d0107c4c828f0
enoquetembe <enoquetembetembe10@gmail.com>
2023-02-26 19:48:48 -0300

    update profile

--- web/src/hooks/auth.jsx
+++ web/src/hooks/auth.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { createContext, useContext } from 'react'
 
-import { api } from  '../services/api'
+import { api } from '../services/api'
 
 export const AuthContext = createContext({})
 
@@ -10,21 +10,20 @@ export { useState, useEffect } from 'react'
 export function AuthProvider({ children }) {
   const [data, setData] = useState({})
 
-  async function signIn({email, password}) {
+  async function signIn({ email, password }) {
     try {
       const response = await api.post('/session', { email, password })
-      const {token, user} = response.data
+      const { token, user } = response.data
 
-      api.defaults.headers.authorization = `Bear ${token}`
-      setData({user, token})
+      api.defaults.headers.common['Authorization'] = `Bear ${token}`
+      setData({ user, token })
 
       localStorage.setItem('@mynotes:user', JSON.stringify(user))
-      localStorage.setItem('mynotes:token', token)
-
-    } catch(error) {
-      if(error.response) {
+      localStorage.setItem('@mynotes:token', token)
+    } catch (error) {
+      if (error.response) {
         alert(error.response.data.message)
-      } else  {
+      } else {
         alert('It was no possible to login')
       }
     }
@@ -32,30 +31,50 @@ export function AuthProvider({ children }) {
 
   function signOut() {
     localStorage.removeItem('@mynotes:user')
-    localStorage.removeItem('mynotes:token')
-
+    localStorage.removeItem('@mynotes:token')
+    
     setData({})
+  }
+
+  async function updateProfile({ user }) {
+    try {
+      await api.put('/users', user)
+      localStorage.setItem('@mynotes:user', JSON.stringify(user))
+      
+      setData({user, token: data.token})
+
+      alert('Profile updated.')
+
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert('It was not possible to update profile.')
+      }
+    }
   }
 
   useEffect(() => {
     const user = localStorage.getItem('@mynotes:user')
-    const token = localStorage.getItem('mynotes:token')
+    const token = localStorage.getItem('@mynotes:token')
 
-    if(user && token) {
-      api.defaults.headers.authorization = `Bear ${token}`
+    if (user && token) {
+      api.defaults.headers.common['Authorization'] = `Bear ${token}`
       setData({
         token,
-        user: JSON.parse(user)
+        user: JSON.parse(user),
       })
     }
-
   }, [])
-  return(
-    <AuthContext.Provider value={{ 
+  return (
+    <AuthContext.Provider
+      value={{
         signIn,
-        signOut, 
-        user: data.user
-      }}>
+        signOut,
+        updateProfile,
+        user: data.user,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   )
--- web/src/pages/Profile/index.jsx
+++ web/src/pages/Profile/index.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import { Container, Form, Avatar } from './styles'
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera  } from 'react-icons/fi'
@@ -6,7 +7,26 @@ import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera  } from 'react-icons/fi'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
+import { useAuth } from '../../hooks/auth'
+
 export function Profile() {
+  const { user, updateProfile } = useAuth()
+  const [name, setName] = useState(user.name)
+  const [email, seEmail] = useState(user.email)
+  const [newPassword, setNewPassword] = useState('')
+  const [oldPassword, setOldPassword] = useState('')
+  
+  async function handleUpdateProfile() {
+    const user = {
+      name,
+      email,
+      password: newPassword,
+      old_password: oldPassword,
+    }
+
+    await updateProfile({user})
+  }
+
   return(
     <Container>
       <header>
@@ -30,27 +50,33 @@ export function Profile() {
           type='text'
           placeholder='Name'
           icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           type='text'
           placeholder='Email'
           icon={FiMail}
+          value={email}
+          onChange={e => seEmail(e.target.value)}
         />
 
         <Input 
           type='password'
           placeholder='Old password'
           icon={FiLock}
+          onChange={e => setOldPassword(e.target.value)}
         />
 
         <Input 
           type='password'
           placeholder='New password'
           icon={FiLock}
+          onChange={e => setNewPassword(e.target.value)}
         />
 
-        <Button title='Save'/>
+        <Button title='Save' onClick={handleUpdateProfile}/>
       </Form>
     </Container>
   )
