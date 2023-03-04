e730bbb16ff169096e95fed7287831a942c58d4c
enoquetembe <enoquetembetembe10@gmail.com>
2023-03-03 21:41:32 -0300

    selected tags

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -7,7 +7,33 @@ import { Input } from '../../components/Input'
 import { Section } from '../../components/Section'
 import { Note } from '../../components/Note'
 
+import { useState, useEffect } from 'react'
+import { api } from '../../services/api'
+
 export function Home() {
+  const [tags, setTags] = useState([])
+  const [selectedTags, setSelectedTags] = useState([])
+
+  function handleSelectedTags(tagName) {
+    const isAlreadySelected = selectedTags.includes(tagName)
+
+    if(isAlreadySelected) {
+      const filteredTags = selectedTags.filter(tag => tag !== tagName)
+      setSelectedTags(filteredTags)
+    } else {
+      setSelectedTags(prevState => [...prevState, tagName])
+    }
+  }
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get('/tags')
+      setTags(response.data)
+    }
+
+    fetchTags()
+  }, [])
+
   return(
     <Container>
       <Brand>
@@ -17,9 +43,23 @@ export function Home() {
       <Header/>
 
       <Menu>
-        <li><ButtonText title='All' isActive/></li>
-        <li><ButtonText title='react'/></li>
-        <li><ButtonText title='node'/></li>
+        <li>
+           <ButtonText 
+              title='All' 
+              isActive={selectedTags.length === 0}
+           />
+        </li>
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText 
+                title={tag.name}
+                onClick={() => handleSelectedTags(tag.name)}
+                isActive={selectedTags.includes(tag.name)}
+              />
+            </li>
+          ))
+        }
       </Menu>
 
       <Search>
