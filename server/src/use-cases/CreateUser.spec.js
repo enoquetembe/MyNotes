54fa141d4033a9bfa203b3448b1b504c77e03eee
enoquetembe <enoquetembetembe10@gmail.com>
2023-03-25 15:46:09 -0300

    testing CreateUser use case

--- server/src/use-cases/CreateUser.spec.js
+++ server/src/use-cases/CreateUser.spec.js
@@ -16,4 +16,26 @@ describe('Create user', () => {
 
     expect(createdUser).toHaveProperty('id')
   })
+
+  it('should not be able to create a user with an existing email', async () => {
+    const user1 = {
+      name: 'user1',
+      email: 'email-example@example.com',
+      password: '1234',
+    }
+
+    const user2 = {
+      name: 'user2',
+      email: 'email-example@example.com',
+      password: '1234',
+    }
+
+    const userRepositoryInMemory = new UserRepositoryInMemory()
+    const createUser = new CreateUser(userRepositoryInMemory)
+
+    await createUser.execute(user1)
+
+    expect(createUser.execute(user2)).rejects.toEqual(new AppError('This email is already in use.'))
+
+  })
 })
